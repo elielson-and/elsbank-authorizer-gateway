2e60aa3c7fd8236aa18e6e4f9c0adee327c76c2c
Elielson André <68979955+elielson-andre@users.noreply.github.com>
2023-06-29 12:21:00 -0300

    test: testing server comunication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/ping', function () {
+    return Response()->json(['message'=>'pong',200]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
