96cd65f61c6680b006d20cd35de4dd00e0478d38
Elielson André <68979955+elielson-andre@users.noreply.github.com>
2023-07-02 00:57:59 -0300

    chore: validating and performing transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -32,13 +32,55 @@ class TransactionController extends Controller
         }
         
         //TODO: join to one consult
-        $payer_balance = floatVal(User::with('wallet')->where('uuid', $request->payer)->first()->wallet->balance);
-        $payee_balance = floatVal(User::with('wallet')->where('uuid', $request->payee)->first()->wallet->balance);
+        $payer_data = User::with('wallet')->where('uuid', $request->payer)->first();
+        $payee_data = User::with('wallet')->where('uuid', $request->payee)->first();
        
-        if ($request->value > $payer_balance){
-            return response()->json(['message'=>"Can't process this transaction. Payer has no suficient founds"],401);
+        if ($request->value > $payer_data->wallet->balance){
+            return response()->json(['message'=>"Unable to process. Payer doesn't have sufficient funds"],401);
         }
 
+        try {
+
+            // Subtracting from the payer
+            $payer = User::where('uuid',$request->payer)->first();
+            $payer->wallet()->update([
+                'balance' => floatval($payer_data->wallet->balance) - floatval($request->value)
+            ]);
+
+            // Crediting the beneficiary
+            $payee = User::where('uuid',$request->payee)->first();
+            $payee->wallet()->update([
+                'balance' => floatval($payee_data->wallet->balance) + floatval($request->value)
+            ]);
+
+            // Register transaction info
+            $transaction = Transaction::create([
+                'uuid' => Str::uuid(),
+                'payer' => $request->payer,
+                'payee' => $request->payee,
+                'value' => $request->value
+            ]);
+
+            return response()->json([
+                ['message' => 'success'],
+                [
+                    'transaction' => [
+                        'id' => $transaction->uuid,
+                        'payer_name' => strtoupper($payer_data->first_name.' '.$payer_data->last_name),
+                        'payee_name' => strtoupper($payee_data->first_name.' '.$payee_data->last_name),
+                        'value' => $request->value,
+                        'finished_at' => $transaction->created_at
+                    ]
+                ]
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'An error has ocurred, please try again later.',
+                'error_details' => $th
+            ], 500);
+        }
+        
 
     }
 }
